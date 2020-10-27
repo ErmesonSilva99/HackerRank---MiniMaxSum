1ca8ed1f1d42f515e20d2fdd821c194bb72cb451
Ermeson Silva Novais <ermeson.android99@gmail.com>
2020-10-27 17:25:38 -0300

    insert function miniMaxSum

---
+++ index.js
@@ -0,0 +1,17 @@
+function miniMaxSum(arr) {
+    arr = arr.sort();
+
+    let max = arr.slice(1, arr.length);
+    let min = arr.slice(0, arr.length-1);
+
+    const sumArray = array => {
+        let total = array.reduce((total, num) => total + num, 0);
+
+        return total;
+    }
+
+    let sumMax = sumArray(max);
+    let sumMin = sumArray(min);
+
+    console.log(sumMin, sumMax);
+}
